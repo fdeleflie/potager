3ef0f4e95ad5d0c42168da92d8d6415d560ab1d2
fdeleflie <fdeleflie@gmail.com>
2026-04-15 17:02:12 +0200

    maj manuel

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  base: '/potager-migration-web/',
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
